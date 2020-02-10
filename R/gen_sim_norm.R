bab836263bb612900b6bde57b59edb19893b46bd
Shravan Vasishth <vasishth.shravan@gmail.com>
2020-02-10 08:19:36 +0100

    modified the subj and item calls

--- R/gen_sim_norm.R
+++ R/gen_sim_norm.R
@@ -1,8 +1,8 @@
 library(MASS)
 gen_sim_norm <- function(dat=NULL,
-                           alpha=NULL,beta=NULL,
-                           Sigma_u=NULL,Sigma_w=NULL,sigma_e=NULL){
-  ## subject random effects:
+                         alpha=NULL,beta=NULL,
+                         Sigma_u=NULL,Sigma_w=NULL,sigma_e=NULL){
+  
   nsubj<-length(unique(dat$subj))
   u<-mvrnorm(n=nsubj,
              mu=c(0,0),Sigma=Sigma_u)
@@ -11,18 +11,23 @@ gen_sim_norm <- function(dat=NULL,
   nitem<-length(unique(dat$item))
   w<-mvrnorm(n=nitem,
              mu=c(0,0),Sigma=Sigma_w)
+  w<-data.frame(itemid=unique(dat$item),w)
   
   ## generate data row by row:
   N<-dim(dat)[1]
   simbase100<-rep(NA,N)
   for(i in 1:N){
+    current_subjid<-dat[i,]$subj
+    current_itemid<-dat[i,]$item
+    
     simbase100[i] <- rnorm(1,alpha +
-                      u[dat[i,]$subj,1] +
-                      w[dat[i,]$item,1] +
-                      (beta+u[dat[i,]$subj,2]+
-                         w[dat[i,]$item,2])*dat$zcloze[i],
-                    sigma_e)}
+                             u[u$subjid==current_subjid,2] +
+                             w[w$itemid==current_itemid,2] +
+                             (beta+
+                                u[u$subjid==current_subjid,3] +
+                                w[w$itemid==current_itemid,3])*dat$zcloze[i],
+                           sigma_e)}
   dat$simbase100<-simbase100
   simdat<-dat
   simdat
-  }
+}
